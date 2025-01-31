4610574affa4ef80c8601b2184e67aee40c97642
tupesadilla-b <thehackperuyou@gmail.com>
2025-01-31 17:39:37 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Directorio de instalación
+INSTALL_DIR="/usr/local/vps"
+
+# Clonar el repositorio
+echo "Clonando el repositorio..."
+git clone https://github.com/usuario/vps_manager.git "$INSTALL_DIR" || {
+    echo "Error al clonar el repositorio."
+    exit 1
+}
+
+# Establecer permisos
+echo "Estableciendo permisos..."
+chmod -R +x "$INSTALL_DIR"
+
+# Crear enlace simbólico para ejecutar desde cualquier lugar
+ln -sf "$INSTALL_DIR/vps_manager.sh" /usr/local/bin/vps_manager
+
+echo "Instalación completada. Use el comando 'vps_manager' para iniciar."
